0c4677b892857d7315f0ae1bfce8cfe183b1a6c6
mP3nn <marco04.pennacchioni@gmail.com>
2021-02-06 00:04:54 +0100

    First rewiev

--- src/Review.svelte
+++ src/Review.svelte
@@ -1,7 +1,7 @@
 <script>
 	export let name;
 	export let rating;
-	export let description1;
+	export let intro;
 	export let image1;
 	export let description2;
 	export let image2;
@@ -17,16 +17,24 @@
 <div class="card">
 	<div class="row">
 		<div class="col-9">
-			<h1>{name} {rating}</h1>
+			<h1>{name}</h1>
 		</div>
 		<div class="col-3">
 			{#if !readMore}<button class="button-primary" on:click={onReadMore}>Read More</button>{:else}<button class="button-danger" on:click={onReadMore}>Read Less</button>{/if}
 		</div>
 	</div>
 	{#if readMore}
-		<p class="my-2">{description1}</p>
 		<img src="{image1}" class="border-radius my-2" alt="">
-		<p class="my-2">{description2}</p>
+		<h2>Introduzione</h2>
+		<p class="my-2">{intro}</p>
 		<img src="{image2}" class="border-radius my-2" alt="">
+		<h2>Selezione di Giochi</h2>
+		<p class="my-2">{description2[0].g_selection}</p>
+		<h2>Sito Web</h2>
+		<p class="my-2">{description2[0].site}</p>
+		<h2>Bonus e Spin gratuiti</h2>
+		<p class="my-2">{description2[0].bonuses}</p>
+		<h2>Conclusioni</h2>
+		<p class="my-2">{description2[0].conclusion}</p>
 	{/if}
 </div>
